74902c9185835ddcd2ca3df04b8caf63492dafa5
alxevvv <83218535+alxevvv@users.noreply.github.com>
2023-05-23 14:15:00 +0600

    pre-publish edits

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+
+const ghPagesBaseUrl = 'https://alxevvv.github.io/app-string-generators';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? ghPagesBaseUrl : '',
   plugins: [vue()],
-})
+});
